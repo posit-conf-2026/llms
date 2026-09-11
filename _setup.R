52392f28faf243e11480ec2b3d6fc8505cc72298
Sara Altman <sarakaialtman@gmail.com>
2026-09-10 21:03:08 -0700

    Some installation fixes: Update chatlas and use `pak::local_install_deps()` (#23)

--- _setup.R
+++ _setup.R
@@ -4,4 +4,4 @@ if (!requireNamespace("pak", quietly = TRUE)) {
   install.packages("pak", repos = sprintf("https://r-lib.github.io/p/pak/stable/%s/%s/%s", .Platform$pkgType, R.Version()$os, R.Version()$arch))
 }
 
-pak::local_install()
+pak::local_install_deps()
